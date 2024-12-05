0c2e333af5d8648bcc769ca0be29ecbacf6fa972
kushdeepwalia <skushdeep5@gmail.com>
2024-12-05 18:32:53 +0530

    inst 2 api added

--- src/adminApiUrl.jsx
+++ src/adminApiUrl.jsx
@@ -1,4 +1,4 @@
-const adminApiUrl = process.env.NODE_ENV === "development" ? 'http://localhost:5000/admin/api/v1/' : 'https://cscanganwadi-c6be713ed90b.herokuapp.com/admin/api/v1/';
-export const apiUrl = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/v1/' : 'https://cscanganwadi-c6be713ed90b.herokuapp.com/api/v1/';
+const adminApiUrl = process.env.NODE_ENV === "development" ? 'http://localhost:5000/admin/api/v1/' : 'https://api.anganwaditest.co.in/admin/api/v1/';
+export const apiUrl = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/v1/' : 'https://api.anganwaditest.co.in/api/v1/';
 
 export default adminApiUrl;
